10f484121e340119fd8d49bcd9146cda188be030
siddhesh <siddheshchandrayan21@gmail.com>
2026-08-19 12:02:56 +0530

    commit by  siddhesh

---
+++ A.java
@@ -0,0 +1,17 @@
+public class A{
+
+    public void m1(){
+
+     System.out.println("m1....A");
+
+  }
+
+  public static void main(String[]args){
+
+    A a = new A();
+    
+      a.m1(); 
+
+}
+
+}
